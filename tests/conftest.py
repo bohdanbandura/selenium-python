03a29cc4951cc35ab8b9b0aaf70d74913b8ed633
bohdanbandura <bohdan.bandura@qatestlab.com>
2024-05-02 13:56:44 +0300

    setup pipeline

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as chrome_options
+from selenium.webdriver.firefox.options import Options as firefox_options
+from selenium.webdriver.edge.options import Options as edge_options
 import pytest
 
 @pytest.fixture(scope='module', params=['chrome', 'firefox', 'edge'])
@@ -6,11 +9,17 @@ def driver(request):
     driver = None
     
     if request.param == 'chrome':
-        driver = webdriver.Chrome()
+        options = chrome_options()
+        options.add_argument('--headless')
+        driver = webdriver.Chrome(options=options)
     elif request.param == 'firefox':
-        driver = webdriver.Firefox()
+        options = firefox_options()
+        options.add_argument('--headless')
+        driver = webdriver.Firefox(options=options)
     elif request.param == 'edge':
-        driver = webdriver.Edge()
+        options = edge_options()
+        options.add_argument('--headless')
+        driver = webdriver.Edge(options=options)
         
     driver.set_window_size(width=1920, height=1080)
     yield driver
